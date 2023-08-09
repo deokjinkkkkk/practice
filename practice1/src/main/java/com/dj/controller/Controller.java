0b071de96fd0aa7e42d1dff025302b1aadf630d6
antan <antan@DESKTOP-6657P6A>
2023-08-09 19:44:23 +0900

    123

--- practice1/src/main/java/com/dj/controller/Controller.java
+++ practice1/src/main/java/com/dj/controller/Controller.java
@@ -3,7 +3,7 @@ package com.dj.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dj.dao.Practice;
@@ -11,14 +11,15 @@ import com.dj.model.Member;
 
 @RestController
 public class Controller {
+	
 	@Autowired
 	Practice practice;
 	
 	
-	@GetMapping("/main")
+	@RequestMapping("/main")
 	public List<Member> main() {
 		List<Member> list = practice.selectList();
-		System.out.println("++++++++++++++++++++");
+		System.out.println(list);
 		return list;
 	}
 }
