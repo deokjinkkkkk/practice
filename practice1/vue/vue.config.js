0c99ea7c4d2b0ea8e5baed6bc4116efcd1a943ef
antan <antan@DESKTOP-6657P6A>
2023-10-26 01:26:05 +0900

    vue 연동 성공

--- practice1/vue/vue.config.js
+++ practice1/vue/vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {  
+  outputDir: "../src/main/resources/static",  		// npm run build 빌드 시 파일이 생성되는 위치
+  indexPath: "index.html",  			// index.html 파일이 생성될 위치
+  devServer: {  					// Back-End, Spring Boot의 내장 was 주소 입력
+  	port: 8081, //vue 개발서버
+    proxy: "http://localhost:8080"  // springBoot was주소 (backend)
+  },  
+  chainWebpack: config => {  
+    const svgRule = config.module.rule("svg");    
+    svgRule.uses.clear();    
+    svgRule.use("vue-svg-loader").loader("vue-svg-loader");  
+  }  
+};
